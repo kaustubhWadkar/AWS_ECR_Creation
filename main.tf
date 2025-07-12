46c9b5a6d2dfc5cfc5dc54cdc45518deee5c1426
Kaustubh Wadkar <151868024+kaustubhWadkar@users.noreply.github.com>
2025-07-12 12:20:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,15 @@
 provider "aws" {
-    region = "us-east-1"
-  
+  region = "us-east-1"
 }
 
+resource "aws_ecr_repository" "terra_ecr" {
+  name = "test-ecr-terraform"
 
-resource "aws_ecr_repository" "terra-ecr" {
-    name = "test-ecr-terrafrom"
-    image_scanning_configuration {
-      scan_on_push = true
-    
-    }
-   tags = {
-      Environment = "uat"
-      
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 
-    }
-
+  tags = {
+    Environment = "uat"
+  }
 }
